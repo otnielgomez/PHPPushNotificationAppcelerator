19333287f6b43cf7cfee4f56dfdcf55370434d4c
Otniel Gomez <otniel.gomez@gmail.com>
2015-11-13 21:16:18 -0600

    Update push.php

--- push.php
+++ push.php
@@ -11,9 +11,9 @@ $fechaEnvio = new DateTime($date." ".$time, $timeZone);
 $fechaEnvio->setTimezone($utc);
 //echo $fechaEnvio->format('Y-m-d\TH:i');
 if ($titulo && $mensaje) {
-    $Curl_Session1 = curl_init('https://api.cloud.appcelerator.com/v1/users/login.json?key=pYxZvtVCUQZ7TEp4H4hKQYWfi54FY5yK');
+    $Curl_Session1 = curl_init('https://api.cloud.appcelerator.com/v1/users/login.json?key=<KEY>');
     curl_setopt ($Curl_Session1, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt ($Curl_Session1, CURLOPT_POSTFIELDS, "login=otniel.gomez@sorteostec.mx&password=qwe123/*-");
+    curl_setopt ($Curl_Session1, CURLOPT_POSTFIELDS, "login=<EMAIL>&password=<PASSWORD>");
     $res = curl_exec ($Curl_Session1);
     $session = json_decode($res);
     if ($programado === "false") {
@@ -25,7 +25,7 @@ if ($titulo && $mensaje) {
         $payload->vibrate = true;
         $payload->sound = "default";
         $obj->payload = $payload;
-        $Curl_Session2 = curl_init('https://api.cloud.appcelerator.com/v1/push_notification/notify.json?key=pYxZvtVCUQZ7TEp4H4hKQYWfi54FY5yK&_session_id='.$session->meta->session_id);
+        $Curl_Session2 = curl_init('https://api.cloud.appcelerator.com/v1/push_notification/notify.json?key=<KEY>K&_session_id='.$session->meta->session_id);
         curl_setopt ($Curl_Session2, CURLOPT_POSTFIELDS, "payload=".json_encode($obj->payload)."&channel=news_alerts&to_ids=everyone");
     }else{
         //SE PROGRAMA NOTIFICACION
@@ -41,7 +41,7 @@ if ($titulo && $mensaje) {
         $schedule->start_time = $fechaEnvio->format('Y-m-d\TH:i');
         $schedule->push_notification = $push_notification;
         $obj->schedule = $schedule;
-        $Curl_Session2 = curl_init('https://api.cloud.appcelerator.com/v1/push_schedules/create.json?key=pYxZvtVCUQZ7TEp4H4hKQYWfi54FY5yK&_session_id='.$session->meta->session_id);
+        $Curl_Session2 = curl_init('https://api.cloud.appcelerator.com/v1/push_schedules/create.json?key=<KEY>K&_session_id='.$session->meta->session_id);
         curl_setopt ($Curl_Session2, CURLOPT_POSTFIELDS, "schedule=".json_encode($obj->schedule)."");
     }
     curl_exec($Curl_Session2);
